907c3ee03aeddc917d6c7418804cf18ba087221e
Mbialas1 <mbialas1@edu.cdv.pl>
2021-11-18 16:00:13 +0100

    End first tutorial

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OFTI_Service.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OFTI_Service.Controllers
+{
+    [Route("api/workers")]
+    public class UsersController : ControllerBase
+    {
+        private UsersWorkerDbContext _UsersWorkerDb;
+        public UsersController(UsersWorkerDbContext usersWorkerDbContext)
+        {
+            _UsersWorkerDb = usersWorkerDbContext;
+        }
+
+        [Route("test")]
+        public ActionResult<IEnumerable<UsersWorker>> GetAll()
+        {
+            var workers = _UsersWorkerDb.UsersWorkers.ToList();
+
+            return Ok(workers);
+        }
+    }
+}
