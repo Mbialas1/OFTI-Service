25b9b65b85742e17d466e5167986ca452b9e4e9a
Mbialas1 <mbialas1@edu.cdv.pl>
2021-11-18 12:18:01 +0100

    First

---
+++ Entities/UsersWorker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OFTI_Service.Entities
+{
+    public class UsersWorker
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string LoginName { get; set; }
+        public bool Master { get; set; }
+        public bool Admin { get; set; }
+        public DateTime LastLogged { get; set; }
+        public DateTime FirstLogged { get; set; }
+    }
+}
---
+++ Entities/UsersWorkerDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace OFTI_Service.Entities
+{
+    public class UsersWorkerDbContext : DbContext
+    {
+        public DbSet<UsersWorker> UsersWorkers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UsersWorker>()
+                .Property(u => u.Name)
+                .IsRequired()
+                .HasMaxLength(25);
+
+            modelBuilder.Entity<UsersWorker>()
+                .Property(u => u.LastName)
+                .IsRequired()
+                .HasMaxLength(25);
+
+            modelBuilder.Entity<UsersWorker>()
+                .Property(u => u.LoginName)
+                .IsRequired()
+                .HasMaxLength(20);
+
+            modelBuilder.Entity<UsersWorker>()
+                .Property(u => u.Master);
+
+            modelBuilder.Entity<UsersWorker>()
+                .Property(u => u.Admin);
+
+            modelBuilder.Entity<UsersWorker>()
+                .Property(u => u.LastLogged)
+                .IsRequired();
+
+            modelBuilder.Entity<UsersWorker>()
+                .Property(u => u.FirstLogged)
+                .IsRequired();
+
+        }
+
+    }
+}
